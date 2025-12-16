90a1912c6ff488268079bff71589b3e2bf08cdcf
Dhnzhan <gdh00549@naver.com>
2025-12-16 17:35:53 +0900

    Feat: login

--- src/main/java/com/korit/post_mini_project_back/service/OAuth2UserService.java
+++ src/main/java/com/korit/post_mini_project_back/service/OAuth2UserService.java
@@ -1,5 +1,7 @@
 package com.korit.post_mini_project_back.service;
 
+import com.korit.post_mini_project_back.entity.User;
+import com.korit.post_mini_project_back.security.PrincipalUser;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -15,6 +17,7 @@ import java.util.Map;
 @Service
 public class OAuth2UserService extends DefaultOAuth2UserService {
 
+
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
@@ -23,14 +26,23 @@ public class OAuth2UserService extends DefaultOAuth2UserService {
         Map<String, Object> attributes = new LinkedHashMap<>();
         Collection<? extends GrantedAuthority> authorities = oAuth2User.getAuthorities();
         String nameAttributeKey = null;
+        User user = null;
 
         if ("NAVER".equalsIgnoreCase(clientName)) {
             Map<String, Object> response = (Map<String, Object>) oAuth2User.getAttributes().get("response");
             attributes.putAll(response);
             nameAttributeKey = "id";
+            user = User.builder()
+                    .oauth2Id((String) response.get("id"))
+                    .name((String) response.get("name"))
+                    .email((String) response.get("email"))
+                    .provider(clientName)
+                    .role(authorities.stream().findFirst().get().toString())
+                    .imgUrl((String) response.get("profile_image"))
+                    .build();
 
         }
-        return new DefaultOAuth2User(authorities, attributes, nameAttributeKey);
+        return new PrincipalUser(authorities, attributes, nameAttributeKey, user);
     }
 
 }
